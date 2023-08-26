c233b7701e97b3833a458bc49789a19f4d2206b7
T14s <122837733+looonnng@users.noreply.github.com>
2023-08-26 13:52:08 -0500

    Take user inputs by typing or clicking on calculator

--- main.js
+++ main.js
@@ -20,7 +20,26 @@ let operator;
 let secNum;
 
 const operate = (op, num1, num2) => {
-    return operators[op](num1, num2);
+  return operators[op](num1, num2);
 };
 
+// DOM Manipulations
 
+const numbers = document.querySelectorAll(".number");
+const currResult = document.querySelector(".current-result");
+const prevResult = document.querySelector(".prev-result");
+console.log(numbers);
+
+numbers.forEach((num) => {
+  num.addEventListener("click", () => {
+    currResult.innerText += num.innerText;
+  });
+});
+
+document.addEventListener("keypress", (e) => {
+  numbers.forEach((num) => {
+    if (e.key == num.innerText) {
+      currResult.innerText += e.key;
+    }
+  });
+});
