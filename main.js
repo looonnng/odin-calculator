a2066d72772dd2b9414bc42ab4860622f6bcf240
T14s <122837733+looonnng@users.noreply.github.com>
2023-09-11 22:06:21 -0500

    Add handle functions for numbers, operators Rework logic to store the two numbers and the operator Rewrite operators object to handle string to int

--- main.js
+++ main.js
@@ -2,44 +2,80 @@
 
 const operators = {
   "+": function (a, b) {
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
   },
   "-": function (a, b) {
-    return a - b;
+    return parseFloat(a) - parseFloat(b);
   },
   "*": function (a, b) {
-    return a * b;
+    return parseFloat(a) * parseFloat(b);
   },
   "/": function (a, b) {
-    return a / b;
+    return parseFloat(a) / parseFloat(b);
   },
 };
 
-let firstNum;
-let operator;
-let secNum;
+let currentNum = "";
+let operator = "";
+let previousNum = "";
 
-const operate = (op, num1, num2) => {
-  return operators[op](num1, num2);
-};
+function calculate(op, prevNum, currNum) {
+  return operators[op](prevNum, currNum);
+}
 
-// DOM Manipulations
+// DOM References
+const interface = document.querySelector(".interface-container");
+const btns = document.querySelectorAll("button");
+const ops = document.querySelectorAll(".operator");
+const nums = document.querySelectorAll(".number");
+const currDisplay = document.querySelector(".current-result");
+const prevDisplay = document.querySelector(".prev-result");
+const equal = document.querySelector("#equal");
 
-const numbers = document.querySelectorAll(".number");
-const currResult = document.querySelector(".current-result");
-const prevResult = document.querySelector(".prev-result");
-console.log(numbers);
-
-numbers.forEach((num) => {
-  num.addEventListener("click", () => {
-    currResult.innerText += num.innerText;
+nums.forEach((num) => {
+  num.addEventListener("click", (e) => {
+    handleNumber(e.target.textContent);
   });
 });
 
-document.addEventListener("keypress", (e) => {
-  numbers.forEach((num) => {
-    if (e.key == num.innerText) {
-      currResult.innerText += e.key;
-    }
+ops.forEach((op) => {
+  op.addEventListener("click", (e) => {
+    handleOperator(e.target.textContent);
   });
 });
+
+equal.addEventListener("click", () => {
+  handleCalculation();
+});
+
+function handleNumber(num) {
+  if (currentNum.length < 11) {
+    currentNum += num;
+    currDisplay.textContent = currentNum;
+  }
+  // Add infinity msg
+}
+
+function handleOperator(op) {
+  // handle if user hit any operator after equal sign
+  if (prevDisplay.textContent) {
+    operator = op;
+    previousNum = currDisplay.textContent;
+    prevDisplay.textContent = `${previousNum} ${operator}`;
+    currentNum = "";
+    currDisplay.textContent = "";
+  } else {
+    operator = op;
+    previousNum = currentNum;
+    prevDisplay.textContent = `${previousNum} ${operator}`;
+    currentNum = "";
+    currDisplay.textContent = "";
+  }
+}
+
+function handleCalculation() {
+  prevDisplay.textContent = `${previousNum} ${operator} ${currentNum}`;
+  currDisplay.textContent = calculate(operator, previousNum, currentNum);
+  currentNum = "";
+  previousNum = currDisplay.textContent;
+}
