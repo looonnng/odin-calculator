fe98f6b2714eeb58174c75652b43bfd6530497a8
T14s <122837733+looonnng@users.noreply.github.com>
2023-10-03 20:00:57 -0500

    Add conditionals to handle different user input cases

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ const operators = {
 let currentNum = "";
 let operator = "";
 let previousNum = "";
+let result = "";
 
 function calculate(op, prevNum, currNum) {
   return operators[op](prevNum, currNum);
@@ -31,6 +32,7 @@ const nums = document.querySelectorAll(".number");
 const currDisplay = document.querySelector(".current-result");
 const prevDisplay = document.querySelector(".prev-result");
 const equal = document.querySelector("#equal");
+const clear = document.querySelector("#clear");
 
 nums.forEach((num) => {
   num.addEventListener("click", (e) => {
@@ -48,6 +50,10 @@ equal.addEventListener("click", () => {
   handleCalculation();
 });
 
+clear.addEventListener("click", () => {
+  handleClear();
+});
+
 function handleNumber(num) {
   if (currentNum.length < 11) {
     currentNum += num;
@@ -59,11 +65,19 @@ function handleNumber(num) {
 function handleOperator(op) {
   // handle if user hit any operator after equal sign
   if (prevDisplay.textContent) {
-    operator = op;
-    previousNum = currDisplay.textContent;
-    prevDisplay.textContent = `${previousNum} ${operator}`;
-    currentNum = "";
-    currDisplay.textContent = "";
+    if (currDisplay.textContent) {
+      if (op == operator) {
+        handleCalculation();
+      } else {
+        operator = op;
+        prevDisplay.textContent = `${previousNum} ${operator}`;
+      }
+    } else {
+      operator = op;
+      prevDisplay.textContent = `${previousNum} ${operator}`;
+      currentNum = "";
+      currDisplay.textContent = "";
+    }
   } else {
     operator = op;
     previousNum = currentNum;
@@ -74,8 +88,20 @@ function handleOperator(op) {
 }
 
 function handleCalculation() {
-  prevDisplay.textContent = `${previousNum} ${operator} ${currentNum}`;
-  currDisplay.textContent = calculate(operator, previousNum, currentNum);
+  prevDisplay.textContent = `${previousNum} ${operator} ${currentNum} = ${calculate(
+    operator,
+    previousNum,
+    currentNum
+  )}`;
+  previousNum = calculate(operator, previousNum, currentNum);
+  currDisplay.textContent = `${previousNum}`;
   currentNum = "";
-  previousNum = currDisplay.textContent;
+}
+
+function handleClear() {
+  currentNum = "";
+  operator = "";
+  previousNum = "";
+  currDisplay.textContent = "";
+  prevDisplay.textContent = "";
 }
