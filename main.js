9af1c811ceae9e760efe6cb0eaa702c63aa1ac2f
T14s <122837733+looonnng@users.noreply.github.com>
2023-10-07 15:57:49 -0500

    Remove modulo button Add clear entry button Modify handleCalculation to fix user double clicking Adjust handleOperator to work with handleClearEntry

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // basic math operators
-
 const operators = {
   "+": function (a, b) {
     return parseFloat(a) + parseFloat(b);
@@ -20,10 +19,6 @@ let operator = "";
 let previousNum = "";
 let result = "";
 
-function calculate(op, prevNum, currNum) {
-  return operators[op](prevNum, currNum);
-}
-
 // DOM References
 const interface = document.querySelector(".interface-container");
 const btns = document.querySelectorAll("button");
@@ -33,7 +28,9 @@ const currDisplay = document.querySelector(".current-result");
 const prevDisplay = document.querySelector(".prev-result");
 const equal = document.querySelector("#equal");
 const clear = document.querySelector("#clear");
+const clearEntry = document.querySelector("#clear-entry");
 
+//Event Listeners
 nums.forEach((num) => {
   num.addEventListener("click", (e) => {
     handleNumber(e.target.textContent);
@@ -54,23 +51,40 @@ clear.addEventListener("click", () => {
   handleClear();
 });
 
+clearEntry.addEventListener("click", () => {
+  handleClearEntry();
+});
+
+//Functions
+function calculate(op, prevNum, currNum) {
+  let result = operators[op](prevNum, currNum);
+  if (result.toString().length > 11) {
+    // Handle decimal numbers
+    return Math.round((result + Number.EPSILON) * 100) / 100;
+  }
+  return result;
+}
+
 function handleNumber(num) {
   if (currentNum.length < 11) {
     currentNum += num;
-    currDisplay.textContent = currentNum;
+    currDisplay.textContent = `${previousNum} ${operator} ${currentNum}`;
   }
-  // Add infinity msg
 }
 
 function handleOperator(op) {
-  // handle if user hit any operator after equal sign
   if (prevDisplay.textContent) {
     if (currDisplay.textContent) {
       if (op == operator) {
         handleCalculation();
+      } else if (previousNum === "") {
+        operator = op;
+        previousNum = currentNum;
+        currDisplay.textContent = `${previousNum} ${operator}`;
+        currentNum = "";
       } else {
         operator = op;
-        prevDisplay.textContent = `${previousNum} ${operator}`;
+        currDisplay.textContent = `${previousNum} ${operator} ${currentNum}`;
       }
     } else {
       operator = op;
@@ -81,27 +95,51 @@ function handleOperator(op) {
   } else {
     operator = op;
     previousNum = currentNum;
-    prevDisplay.textContent = `${previousNum} ${operator}`;
+    currDisplay.textContent = `${previousNum} ${operator}`;
     currentNum = "";
-    currDisplay.textContent = "";
   }
 }
 
 function handleCalculation() {
-  prevDisplay.textContent = `${previousNum} ${operator} ${currentNum} = ${calculate(
-    operator,
-    previousNum,
-    currentNum
-  )}`;
-  previousNum = calculate(operator, previousNum, currentNum);
-  currDisplay.textContent = `${previousNum}`;
-  currentNum = "";
+  if (currentNum === "0" && operator === "/") {
+    // user input number is typeof string at first - fix doubling clicking operations that lead to "ERROR"
+    handleDivideByZero();
+  } else if (currentNum && operator && previousNum) {
+    prevDisplay.textContent = `${previousNum} ${operator} ${currentNum} = ${calculate(
+      operator,
+      previousNum,
+      currentNum
+    )}`;
+    previousNum = calculate(operator, previousNum, currentNum);
+    currDisplay.textContent = `${previousNum} ${operator}`;
+    currentNum = "";
+  }
 }
 
 function handleClear() {
+  currDisplay.textContent = "";
+  prevDisplay.textContent = "";
   currentNum = "";
   operator = "";
   previousNum = "";
-  currDisplay.textContent = "";
+}
+
+function handleClearEntry() {
+  if (currentNum) {
+    currentNum = "";
+    currDisplay.textContent = `${previousNum} ${operator}`;
+  } else {
+    currDisplay.textContent = "";
+    currentNum = "";
+    previousNum = "";
+    operator = "";
+  }
+}
+
+function handleDivideByZero() {
+  currDisplay.textContent = "ERROR";
   prevDisplay.textContent = "";
+  currentNum = "";
+  operator = "";
+  previousNum = "";
 }
